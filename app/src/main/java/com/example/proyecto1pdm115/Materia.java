481d4efbb44c8affd11fdc650f9820ae0c6f688d
Alex Hernández <hg16037@ues.edu.sv>
2022-05-06 12:43:39 -0600

    Agregando clase Materia

--- app/src/main/java/com/example/proyecto1pdm115/Materia.java
+++ app/src/main/java/com/example/proyecto1pdm115/Materia.java
@@ -1,14 +1,4 @@
 package com.example.proyecto1pdm115;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-
-public class Materia extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_materia);
-    }
+public class Materia {
 }
