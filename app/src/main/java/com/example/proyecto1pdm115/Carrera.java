916f1ba7dbecd51442174d4e2e18dd459d4f6fa8
Montes-Tobar <mt17005@ues.edu.sv>
2022-05-07 10:32:17 -0600

    Insertar carrera

--- app/src/main/java/com/example/proyecto1pdm115/Carrera.java
+++ app/src/main/java/com/example/proyecto1pdm115/Carrera.java
@@ -1,4 +1,30 @@
 package com.example.proyecto1pdm115;
 
 public class Carrera {
+    private String id_carrera;
+    private String nombre_carrera;
+
+    public Carrera(){
+    }
+
+    public Carrera(String id_carrera, String nombre_carrera) {
+        this.id_carrera = id_carrera;
+        this.nombre_carrera = nombre_carrera;
+    }
+
+    public String getNombre_carrera() {
+        return nombre_carrera;
+    }
+
+    public void setNombre_carrera(String nombre_carrera) {
+        this.nombre_carrera = nombre_carrera;
+    }
+
+    public String getId_carrera() {
+        return id_carrera;
+    }
+
+    public void setId_carrera(String id_carrera) {
+        this.id_carrera = id_carrera;
+    }
 }
