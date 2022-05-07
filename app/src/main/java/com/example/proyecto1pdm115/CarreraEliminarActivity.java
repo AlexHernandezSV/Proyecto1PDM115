78352cd8b20849ed01055d9f87a26fe364b4fdcd
Montes-Tobar <mt17005@ues.edu.sv>
2022-05-07 12:17:30 -0600

    Eliminar carrera

---
+++ app/src/main/java/com/example/proyecto1pdm115/CarreraEliminarActivity.java
@@ -0,0 +1,30 @@
+package com.example.proyecto1pdm115;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class CarreraEliminarActivity extends AppCompatActivity {
+    //ControlBDMT17005 controlhelper;
+    EditText editId_carrera;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carrera_eliminar);
+        //controlhelper=new ControlBDMT17005 (this);
+        editId_carrera=(EditText)findViewById(R.id.editId_carrera);
+    }
+    public void eliminarCarrera(View v){
+        String regEliminadas;
+        Carrera carrera=new Carrera();
+        carrera.setId_carrera(editId_carrera.getText().toString());
+        //controlhelper.abrir();
+        //regEliminadas=controlhelper.eliminar(carrera);
+        //controlhelper.cerrar();
+        //Toast.makeText(this, regEliminadas, Toast.LENGTH_SHORT).show();
+    }
+}
