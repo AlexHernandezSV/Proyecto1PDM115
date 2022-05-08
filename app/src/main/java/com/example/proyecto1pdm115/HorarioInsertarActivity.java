b0526bba9b9b85d380fee67269144d7828d6b3f2
Montes-Tobar <mt17005@ues.edu.sv>
2022-05-08 16:01:15 -0600

    Insertar horario

---
+++ app/src/main/java/com/example/proyecto1pdm115/HorarioInsertarActivity.java
@@ -0,0 +1,47 @@
+package com.example.proyecto1pdm115;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Date;
+
+public class HorarioInsertarActivity extends AppCompatActivity {
+    //ControlBDMT17005 helper;
+    EditText editId_horario;
+    EditText editDesde_horario;
+    EditText editHasta_horario;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_horario_insertar);
+        //helper = new ControlBDMT17005(this);
+        editId_horario = (EditText) findViewById(R.id.editId_horario);
+        editDesde_horario = (EditText) findViewById(R.id.editDesde_horario);
+        editHasta_horario = (EditText) findViewById(R.id.editHasta_horario);
+    }
+
+    public void insertarHorario(View v) {
+        Integer id_horario=Integer.valueOf(editId_horario.getText().toString());
+        Date desde_horario= (Date) editDesde_horario.getText();
+        Date hasta_horario= (Date) editHasta_horario.getText();
+        //String regInsertados;
+        Horario horario=new Horario();
+        horario.setId_horario(id_horario);
+        horario.setDesde_horario(desde_horario);
+        horario.setHasta_horario(hasta_horario);
+        //helper.abrir();
+        //regInsertados=helper.insertar(horario);
+        //helper.cerrar();
+        //Toast.makeText(this, regInsertados, Toast.LENGTH_SHORT).show();
+    }
+    public void limpiarTexto(View v) {
+        editId_horario.setText("");
+        editDesde_horario.setText("");
+        editHasta_horario.setText("");
+    }
+}
