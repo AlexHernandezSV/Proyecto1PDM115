86e9924b584d74b70ecf9422fb7d47003cb076e3
Montes-Tobar <mt17005@ues.edu.sv>
2022-05-10 20:00:22 -0600

    DetalleOferta actualizar

--- app/src/main/java/com/example/proyecto1pdm115/DetalleOfertaActualizarActivity.java
+++ app/src/main/java/com/example/proyecto1pdm115/DetalleOfertaActualizarActivity.java
@@ -1,22 +1,23 @@
 package com.example.proyecto1pdm115;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.app.Activity;
 import android.os.Bundle;
+import android.app.Activity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class DetalleOfertaInsertarActivity extends AppCompatActivity {
+public class DetalleOfertaActualizarActivity extends AppCompatActivity {
     //ControlBDMT17005 helper;
     EditText editGrupo;
     EditText editId_materias_activas;
     EditText editId_aula;
     EditText editCant_inscritos;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detalle_oferta_insertar);
+        setContentView(R.layout.activity_detalle_oferta_actualizar);
         //helper = new ControlBDMT17005(this);
         editGrupo = (EditText) findViewById(R.id.editGrupo);
         editId_materias_activas = (EditText) findViewById(R.id.editId_materias_activas);
@@ -24,21 +25,16 @@ public class DetalleOfertaInsertarActivity extends AppCompatActivity {
         editCant_inscritos = (EditText) findViewById(R.id.editCant_inscritos);
     }
 
-    public void insertarDetalleOferta(View v) {
-        //String regInsertados;
-        String grupo=editGrupo.getText().toString();
-        String id_materias_activas=editId_materias_activas.getText().toString();
-        Integer id_aula=Integer.valueOf(editId_aula.getText().toString());
-        Integer cant_inscritos=Integer.valueOf(editCant_inscritos.getText().toString());
-        DetalleOferta detalleOferta= new DetalleOferta();
-        detalleOferta.setGrupo(grupo);
-        detalleOferta.setId_materias_activas(id_materias_activas);
-        detalleOferta.setId_aula(id_aula);
-        detalleOferta.setCant_inscritos(cant_inscritos);
+    public void actualizarDetalleOferta(View v) {
+        DetalleOferta detalleOferta = new DetalleOferta();
+        detalleOferta.setGrupo(editGrupo.getText().toString());
+        detalleOferta.setId_materias_activas(editId_materias_activas.getText().toString());
+        detalleOferta.setId_aula(Integer.valueOf(editId_aula.getText().toString()));
+        detalleOferta.setCant_inscritos(Integer.valueOf(editCant_inscritos.getText().toString()));
         //helper.abrir();
-        //regInsertados=helper.insertar(detalleOferta);
+        //String estado = helper.actualizar(detalleOferta);
         //helper.cerrar();
-        //Toast.makeText(this, regInsertados, Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, estado, Toast.LENGTH_SHORT).show();
     }
 
     public void limpiarTexto(View v) {
