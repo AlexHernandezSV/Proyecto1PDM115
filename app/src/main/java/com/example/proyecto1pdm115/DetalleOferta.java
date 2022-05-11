18679a9920fdaa025d4088f70bb9203947446546
Montes-Tobar <mt17005@ues.edu.sv>
2022-05-10 18:57:50 -0600

    DetalleOferta insertar

---
+++ app/src/main/java/com/example/proyecto1pdm115/DetalleOferta.java
@@ -0,0 +1,50 @@
+package com.example.proyecto1pdm115;
+
+public class DetalleOferta {
+    private String grupo;
+    private String id_materias_activas;
+    private Integer id_aula;
+    private Integer cant_inscritos;
+
+    public DetalleOferta(){
+    }
+
+    public DetalleOferta(String grupo, String id_materias_activas, Integer id_aula, Integer cant_inscritos) {
+        this.grupo = grupo;
+        this.id_materias_activas = id_materias_activas;
+        this.id_aula = id_aula;
+        this.cant_inscritos = cant_inscritos;
+    }
+
+    public String getGrupo() {
+        return grupo;
+    }
+
+    public void setGrupo(String grupo) {
+        this.grupo = grupo;
+    }
+
+    public String getId_materias_activas() {
+        return id_materias_activas;
+    }
+
+    public void setId_materias_activas(String id_materias_activas) {
+        this.id_materias_activas = id_materias_activas;
+    }
+
+    public Integer getId_aula() {
+        return id_aula;
+    }
+
+    public void setId_aula(Integer id_aula) {
+        this.id_aula = id_aula;
+    }
+
+    public Integer getCant_inscritos() {
+        return cant_inscritos;
+    }
+
+    public void setCant_inscritos(Integer cant_inscritos) {
+        this.cant_inscritos = cant_inscritos;
+    }
+}
---
+++ app/src/main/java/com/example/proyecto1pdm115/DetalleOfertaInsertarActivity.java
@@ -0,0 +1,49 @@
+package com.example.proyecto1pdm115;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class DetalleOfertaInsertarActivity extends AppCompatActivity {
+    //ControlBDMT17005 helper;
+    EditText editGrupo;
+    EditText editId_materias_activas;
+    EditText editId_aula;
+    EditText editCant_inscritos;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_oferta_insertar);
+        editGrupo = (EditText) findViewById(R.id.editGrupo);
+        editId_materias_activas = (EditText) findViewById(R.id.editId_materias_activas);
+        editId_aula = (EditText) findViewById(R.id.editId_aula);
+        editCant_inscritos = (EditText) findViewById(R.id.editCant_inscritos);
+    }
+
+    public void insertarDetalleOferta(View v) {
+        //String regInsertados;
+        String grupo=editGrupo.getText().toString();
+        String id_materias_activas=editId_materias_activas.getText().toString();
+        Integer id_aula=Integer.valueOf(editId_aula.getText().toString());
+        Integer cant_inscritos=Integer.valueOf(editCant_inscritos.getText().toString());
+        DetalleOferta detalleOferta= new DetalleOferta();
+        detalleOferta.setGrupo(grupo);
+        detalleOferta.setId_materias_activas(id_materias_activas);
+        detalleOferta.setId_aula(id_aula);
+        detalleOferta.setCant_inscritos(cant_inscritos);
+        //helper.abrir();
+        //regInsertados=helper.insertar(detalleOferta);
+        //helper.cerrar();
+        //Toast.makeText(this, regInsertados, Toast.LENGTH_SHORT).show();
+    }
+
+    public void limpiarTexto(View v) {
+        editGrupo.setText("");
+        editId_materias_activas.setText("");
+        editId_aula.setText("");
+        editCant_inscritos.setText("");
+    }
+}
