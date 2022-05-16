928b515af89c7afc7bb933f82bd7827e2f01edcf
Alex Hernández <hg16037@ues.edu.sv>
2022-05-16 13:57:47 -0600

    CRUD materia completo

--- app/src/main/java/com/example/proyecto1pdm115/MateriaActualizarActivity.java
+++ app/src/main/java/com/example/proyecto1pdm115/MateriaActualizarActivity.java
@@ -3,12 +3,47 @@ package com.example.proyecto1pdm115;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MateriaActualizarActivity extends AppCompatActivity {
+public class MateriaActualizarActivity extends Activity {
+
+    //ControlBDCarnet helper;
+    EditText editId_materia;
+    EditText editId_escuela;
+    EditText editNom_materia;
+    EditText editCiclo_materia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_materia_actualizar);
+        //helper = new ControlBDProyecto1PDM115(this);
+        editId_materia = (EditText) findViewById(R.id.editId_materia);
+        editId_escuela = (EditText) findViewById(R.id.editId_escuela);
+        editNom_materia = (EditText) findViewById(R.id.editNom_materia);
+        editCiclo_materia = (EditText) findViewById(R.id.editCiclo_materia);
+
+    }
+
+    public void actualizarMateria(View v) {
+        Materia materia = new Materia();
+        materia.setId_materia(editId_materia.getText().charAt(0));
+        materia.setId_escuela(Integer.parseInt(editId_escuela.getText().toString()));
+        materia.setNombre_materia(editNom_materia.getText().toString());
+        materia.setCiclo_materia(Integer.parseInt(editCiclo_materia.getText().toString()));
+        //helper.abrir();
+        //String estado = helper.actualizar(materia);
+        //helper.cerrar();
+        //Toast.makeText(this, estado, Toast.LENGTH_SHORT).show();
+    }
+
+    public void limpiarTexto(View v) {
+        editId_materia.setText("");
+        editId_escuela.setText("");
+        editNom_materia.setText("");
+        editCiclo_materia.setText("");
     }
 }
--- app/src/main/java/com/example/proyecto1pdm115/MateriaEliminarActivity.java
+++ app/src/main/java/com/example/proyecto1pdm115/MateriaEliminarActivity.java
@@ -2,13 +2,34 @@ package com.example.proyecto1pdm115;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MateriaEliminarActivity extends AppCompatActivity {
+
+public class MateriaEliminarActivity extends Activity {
+
+    //ControlBDCarnet helper;
+    EditText editId_materia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_materia_eliminar);
+        //controlhelper=new ControlBDProyecto1PDM115(this);
+        editId_materia=(EditText)findViewById(R.id.editId_materia);
     }
+
+    public void eliminarMateria(View v){
+        String regEliminadas;
+        Materia materia=new Materia();
+        materia.setId_materia(editId_materia.getText().charAt(0));
+        //controlhelper.abrir();
+        //regEliminadas=controlhelper.eliminar(materia);
+        //controlhelper.cerrar();
+        //Toast.makeText(this, regEliminadas, Toast.LENGTH_SHORT).show();
+    }
+
 }
