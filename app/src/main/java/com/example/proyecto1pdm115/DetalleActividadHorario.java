35ddf8a37e1dfc1d977b1c0278d3355e7a2a28b8
Francisco Santamaria <sm19035@ues.edu.sv>
2022-05-26 18:02:32 -0600

    cambiando int a String

--- app/src/main/java/com/example/proyecto1pdm115/DetalleActividadHorario.java
+++ app/src/main/java/com/example/proyecto1pdm115/DetalleActividadHorario.java
@@ -1,30 +1,30 @@
 package com.example.proyecto1pdm115;
 
 public class DetalleActividadHorario {
-    private int id_horario;
-    private int id_actividad;
+    private String id_horario;
+    private String id_actividad;
 
     public DetalleActividadHorario() {
     }
 
-    public DetalleActividadHorario(int id_horario, int id_actividad) {
+    public DetalleActividadHorario(String id_horario, String id_actividad) {
         this.id_horario = id_horario;
         this.id_actividad = id_actividad;
     }
 
-    public int getId_horario() {
+    public String getId_horario() {
         return id_horario;
     }
 
-    public int getId_actividad() {
+    public String getId_actividad() {
         return id_actividad;
     }
 
-    public void setId_horario(int id_horario) {
+    public void setId_horario(String id_horario) {
         this.id_horario = id_horario;
     }
 
-    public void setId_actividad(int id_actividad) {
+    public void setId_actividad(String id_actividad) {
         this.id_actividad = id_actividad;
     }
 }
