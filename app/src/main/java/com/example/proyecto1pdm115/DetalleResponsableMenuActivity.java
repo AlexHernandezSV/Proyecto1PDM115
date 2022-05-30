9dd66ff741c74d0fb3b70f875e1a7825d3ff9890
diegodc01 <cc17018@ues.edu.sv>
2022-05-29 22:55:35 -0600

    Cambio color

--- app/src/main/java/com/example/proyecto1pdm115/DetalleResponsableMenuActivity.java
+++ app/src/main/java/com/example/proyecto1pdm115/DetalleResponsableMenuActivity.java
@@ -20,7 +20,7 @@ public class DetalleResponsableMenuActivity extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ListView listView = getListView();
-        listView.setBackgroundColor(Color.rgb(0, 0, 255));
+        listView.setBackgroundColor(Color.rgb(255, 255, 255));
         ArrayAdapter<String> adapter = new
                 ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, menu);
         setListAdapter(adapter);
